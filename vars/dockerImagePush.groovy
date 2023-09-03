d4c5e23e3bae2009cb39c4a9528e177f65f2dc59
yvinvijay <129211675+yvinvijay@users.noreply.github.com>
2023-09-04 01:40:13 +0530

    Update dockerImagePush.groovy

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -14,7 +14,9 @@
 def call(String aws_account_id, String region, String ecr_repoName){
     
     sh """
-     aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
-     docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+    aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 434093523956.dkr.ecr.us-west-2.amazonaws.com
+    // aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+    docker push 434093523956.dkr.ecr.us-west-2.amazonaws.com/vinayakyadav:latest
+    // docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
     """
 }
