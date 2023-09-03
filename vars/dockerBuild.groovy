90fa78ddeb5862f8ba65ec53728b9f889cbd8b1e
yvinvijay <129211675+yvinvijay@users.noreply.github.com>
2023-09-04 02:21:37 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -14,4 +14,3 @@ def call(String aws_account_id, String region, String ecr_repoName){
      docker tag ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
     """
 }
-
