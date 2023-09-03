263126c1cb144a83f70df750e4ae32a4c54bf2b8
yvinvijay <129211675+yvinvijay@users.noreply.github.com>
2023-09-04 01:49:06 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -10,10 +10,10 @@
 def call(String aws_account_id, String region, String ecr_repoName){
     
     sh """
-     // docker build -t ${ecr_repoName} .
-     docker build -t vinayakyadav .
-     docker tag vinayakyadav:latest 434093523956.dkr.ecr.us-west-2.amazonaws.com/vinayakyadav:latest
-     // docker tag ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+     docker build -t ${ecr_repoName} .
+     // docker build -t vinayakyadav .
+     // docker tag vinayakyadav:latest 434093523956.dkr.ecr.us-west-2.amazonaws.com/vinayakyadav:latest
+     docker tag ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
     """
 }
 
